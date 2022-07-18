ef417fc16240facce018c4eff641ac1dcb3acbc5
eeenlighten <108919493+eeenlighten@users.noreply.github.com>
2022-07-18 14:15:27 +0900

    2022.07.18

---
+++ src/components/Contents.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// 데이터 저장, 변수, 상수, 객체 ,배열, 리액트는 props
+
+function Contents({ children }) {
+  return <main id="main">{children}</main>;
+}
+
+export default Contents;
+
